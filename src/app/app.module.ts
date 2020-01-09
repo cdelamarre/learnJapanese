e2e47ae4706a29ec95fa76ec79f5924514be3afd
cdelamarre <cdelamarre@e-solutions.tm.fr>
2020-01-10 00:22:06 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
